8e029ee4e8d4e347c13cf9171530b9ac5841a37b
praytic <chernogorovvm@gmail.com>
2016-04-01 21:52:18 +0300

    Change repository structure

---
+++ ConsoleApplication1/DataStructure/BinaryTree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1.DataStructure
+{
+    public class BinaryTree<T>
+    {
+        protected Node<T> Tree;
+
+        public BinaryTree()
+        {
+            Tree = null;
+        }
+
+        public BinaryTree(Node<T> node)
+        {
+            Tree = node;
+        }
+
+        public BinaryTree(List<T> nodeList)
+        {
+            foreach (var node in nodeList)
+            {
+                Add(node);
+            }
+        }
+
+        public void Add(T value)
+        {
+            Node<T>.Add(ref Tree, value);
+        }
+
+        public virtual void AddAndBalance(T value)
+        {
+            Add(value);
+            if (!IsBalanced())
+            {
+                //Console.WriteLine("WAS BALANSED");
+                Balance();
+            }
+        }
+
+        public void Preorder()
+        {
+            Node<T>.Preorder(Tree);
+        }
+
+        public void Inorder()
+        {
+            Node<T>.Inorder(Tree);
+        }
+
+        public void Postorder()
+        {
+            Node<T>.Postorder(Tree);
+        }
+
+        public virtual T Search(T value)
+        {
+            Node<T> node;
+            Node<T>.Search(Tree, value, out node);
+            Node<T>.Print(node);
+            return node.Value;
+        }
+
+        public void Delete(T value)
+        {
+            Node<T>.Delete(ref Tree, value);
+        }
+
+        public virtual void DeleteAndBalance(T value)
+        {
+            Delete(value);
+            if (!IsBalanced())
+            {
+                //Console.WriteLine("WAS BALANSED");
+                Balance();
+            }
+        }
+
+        public virtual void SearchToRoot(T value)
+        {
+            Node<T>.SearchToRoot(ref Tree, value);
+        }
+
+        public virtual void InsertToRoot(T value)
+        {
+            Node<T>.InsertToRoot(ref Tree, value);
+        }
+
+        public virtual void Balance()
+        {
+            Node<T>.Balance(ref Tree);
+        }
+
+        public virtual void InsertRandom(T value)
+        {
+            var rnd = new Random();
+            Node<T>.InsertRandom(ref Tree, value, rnd);
+        }
+
+        public bool IsBalanced()
+        {
+            return Node<T>.IsBalanced(Tree);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ConsoleApplication1.DataStructure;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var charMap = new Dictionary<char, int>();
+            var finalCodeMap = new Dictionary<char, string>();
+
+            using (var sr = new StreamReader("../../Resource/input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (line == null) continue;
+                    var readLine = line.ToCharArray();
+                    foreach (var charValue in readLine)
+                    {
+                        if (charMap.ContainsKey(charValue))
+                        {
+                            charMap[charValue]++;
+                        }
+                        else
+                        {
+                            charMap.Add(charValue, 1);
+                        }
+                    }
+                }
+            }
+            var charSortedMap = charMap.OrderBy(a => a.Value);
+            var charSortedList = new List<Node<int>>();
+            foreach (var valuePair in charSortedMap)
+            {
+                charSortedList.Add(new Node<int>(valuePair.Value));
+            }
+
+            Node<int> mergeNode = (charSortedList.Count == 0) ? null : charSortedList[0];
+            while (charSortedList.Count > 1)
+            {
+                var leftNode = charSortedList[0];
+                var rightNode = charSortedList[1];
+                mergeNode = new Node<int>(leftNode.Value + rightNode.Value);
+                Node<int>.AddNodeLeft(ref mergeNode, leftNode);
+                Node<int>.AddNodeRight(ref mergeNode, rightNode);
+                charSortedList.RemoveRange(0, 2);
+                charSortedList.Add(mergeNode);
+                charSortedList.Sort((a, b) => a.Value.CompareTo(b.Value));
+            }
+            var charCodedList = new List<string>();
+            Node<int>.GetCodeList(mergeNode, ref charCodedList);
+            charCodedList.Reverse();
+            int index = 0;
+            foreach (var mapEntry in charSortedMap)
+            {
+                finalCodeMap[mapEntry.Key] = charCodedList[index++];
+            }
+            foreach (var code in finalCodeMap)
+            {
+                Console.WriteLine(code);
+            }
+        }
+    }
+}
