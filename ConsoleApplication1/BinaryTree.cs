fcae6b5e0f4a6ca764ad17351166490728658755
praytic <chernogorovvm@gmail.com>
2016-04-01 21:52:18 +0300

    Change repository structure

---
+++ ConsoleApplication1/BinaryTree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1 {
+    public class BinaryTree<T> {
+        protected Node<T> Tree;
+
+        public BinaryTree() {
+            Tree = null;
+        }
+
+        public BinaryTree(Node<T> node) {
+            Tree = node;
+        }
+
+        public BinaryTree(List<T> nodeList) {
+            foreach (var node in nodeList) {
+                Node<T>.Add(ref Tree, node);
+            }
+        }
+
+        public void Add(T value) {
+            Node<T>.Add(ref Tree, value);
+        }
+
+        public void AddAndBalance(T value) {
+            Node<T>.Add(ref Tree, value);
+            if (!Node<T>.IsBalanced(Tree)) {
+                //Console.WriteLine("WAS BALANSED");
+                Node<T>.Balance(ref Tree);
+            }
+        }
+
+        public void Preorder() {
+            Node<T>.Preorder(Tree);
+        }
+
+        public void Inorder() {
+            Node<T>.Inorder(Tree);
+        }
+
+        public void Postorder() {
+            Node<T>.Postorder(Tree);
+        }
+
+        public T Search(T value) {
+            Node<T> node;
+            Node<T>.Search(Tree, value, out node);
+            Node<T>.Print(node);
+            return node.Value;
+        }
+
+        public void Delete(T value) {
+            Node<T>.Delete(ref Tree, value);
+        }
+
+        public void DeleteAndBalance(T value) {
+            Node<T>.Delete(ref Tree, value);
+            if (!Node<T>.IsBalanced(Tree)) {
+                //Console.WriteLine("WAS BALANSED");
+                Node<T>.Balance(ref Tree);
+            }
+        }
+
+        public void SearchToRoot(T value) {
+            Node<T>.SearchToRoot(ref Tree, value);
+        }
+
+        public void InsertToRoot(T value) {
+            Node<T>.InsertToRoot(ref Tree, value);
+        }
+
+        public void Balance() {
+            Node<T>.Balance(ref Tree);
+        }
+
+        public void InsertRandom(T value) {
+            var rnd = new Random();
+            Node<T>.InsertRandom(ref Tree, value, rnd);
+        }
+
+        public bool IsBalanced() {
+            return Node<T>.IsBalanced(Tree);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace ConsoleApplication1 {
+
+    public class Program {
+        static void Main() {
+            List<int> inputList = new List<int>();
+            using (StreamReader sr = new StreamReader("../../input.txt")) {
+                inputList = sr
+                    .ReadLine()
+                    .Split(',')
+                    .Select(int.Parse)
+                    .ToList();
+            }
+            BinaryTree<int> tree = new BinaryTree<int>();
+            foreach (int inputValue in inputList) {
+                tree.Add(inputValue);
+            }
+            tree.Postorder();
+            foreach (int inputValue in inputList) {
+                if (inputValue % 2 == 1) {
+                    tree.Delete(inputValue);
+                }
+            }
+            Console.WriteLine();
+            tree.Postorder();
+        }
+    }
+}
+
