42c7a1a7f19083a57752e80d7c32af54ed3c931a
praytic <chernogorovvm@gmail.com>
2016-04-16 20:23:15 +0300

    Complete graph logic

--- graph/DataStructure/AdjacencyMatrix.cs
+++ graph/DataStructure/AdjacencyMatrix.cs
@@ -1,7 +1,88 @@
-﻿namespace graph.DataStructure
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace graph.DataStructure
 {
-    public class AdjacencyMatrix
+    public class AdjacencyMatrix<TValue, TCost> : IEnumerable
     {
-         
+        private int Size { get; set; }
+
+        private Dictionary<TValue, int> Schema { get; }
+
+        private TCost[,] Matrix { get; set; }
+
+        public AdjacencyMatrix()
+        {
+            Schema = new Dictionary<TValue, int>();
+            Matrix = new TCost[0, 0];
+        }
+
+        public bool Contains(TValue value)
+        {
+            return Schema.ContainsKey(value);
+        }
+
+        public void Add(TValue value)
+        {
+            Schema.Add(value, Size);
+            Size++;
+            Matrix = ResizeArray(Matrix, Size, Size);
+        }
+
+        public void Remove(TValue value) {
+            Schema.Remove(value);
+            Size--;
+            Matrix = ResizeArray(Matrix, Size, Size);
+        }
+
+        public void AddDirectedEdge(TValue @from, TValue to, TCost cost)
+        {
+            Matrix[Schema[from], Schema[to]] = cost;
+        }
+
+        public void AddUndirectedEdge(TValue @from, TValue to, TCost cost) {
+            Matrix[Schema[from], Schema[to]] = cost;
+            Matrix[Schema[to], Schema[from]] = cost;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Matrix.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder matrix = new StringBuilder();
+            var schema = Schema.Keys.ToList();
+           /* matrix.Append(string.Format("{0, 20} | ", ' '));
+            for (int i = 0; i < Size; i++)
+            {
+                matrix.Append(schema[i].ToString()[0] + " ");
+            }
+            matrix.AppendLine();*/
+            for (int i = 0; i < Size; i++)
+            {
+                matrix.Append(string.Format("{0, 20} | ", schema[i]));
+                for (int j = 0; j < Size; j++)
+                {
+                    matrix.Append(Matrix[i, j] + " ");
+                }
+                matrix.AppendLine();
+            }
+            return matrix.ToString();
+        }
+
+        private T[,] ResizeArray<T>(T[,] original, int rows, int cols) {
+            var newArray = new T[rows, cols];
+            int minRows = Math.Min(rows, original.GetLength(0));
+            int minCols = Math.Min(cols, original.GetLength(1));
+            for (int i = 0; i < minRows; i++)
+                for (int j = 0; j < minCols; j++)
+                    newArray[i, j] = original[i, j];
+            return newArray;
+        }
     }
 }
---
+++ graph/DataStructure/Graph.cs
@@ -0,0 +1,46 @@
+﻿namespace graph.DataStructure {
+
+    public class Graph<TValue> {
+        private readonly AdjacencyMatrix<TValue, byte> _adjacencyMatrix;
+
+        public Graph() {
+            _adjacencyMatrix = new AdjacencyMatrix<TValue, byte>();
+        }
+
+        public Graph(AdjacencyMatrix<TValue, byte> adjacencyMatrix) {
+            _adjacencyMatrix = adjacencyMatrix;
+        }
+
+        public void AddNode(TValue value) {
+            _adjacencyMatrix.Add(value);
+        }
+
+        public void AddDirectedEdge(TValue from, TValue to) {
+            AddDirectedEdge(from, to, 1);
+        }
+
+        public void AddDirectedEdge(TValue from, TValue to, byte cost) {
+            _adjacencyMatrix.AddDirectedEdge(from, to, 1);
+        }
+
+        public void AddUndirectedEdge(TValue from, TValue to) {
+            AddUndirectedEdge(from, to, 1);
+        }
+
+        public void AddUndirectedEdge(TValue from, TValue to, byte cost) {
+            _adjacencyMatrix.AddUndirectedEdge(from, to, cost);
+        }
+
+        public bool Contains(TValue value) {
+            return _adjacencyMatrix.Contains(value);
+        }
+
+        public void Remove(TValue value) {
+            _adjacencyMatrix.Remove(value);
+        }
+
+        public override string ToString() {
+            return _adjacencyMatrix.ToString();
+        }
+    }
+}
