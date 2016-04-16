d185f46fba6e4bf8d06a233a0e07c3f661d7c988
praytic <chernogorovvm@gmail.com>
2016-04-16 11:46:05 +0300

    Create 'csharp-5' branch

---
+++ graph/DataStructure/Graph.cs
@@ -0,0 +1,90 @@
+﻿namespace graph.DataStructure
+{
+    public class Graph<T> {
+        private readonly NodeList<T> _nodeSet;
+
+        public Graph() : this(null)
+        {
+            _nodeSet = new NodeList<T>();
+        }
+        public Graph(NodeList<T> nodeSet) {
+            _nodeSet = nodeSet;
+        }
+
+        public void AddNode(GraphNode<T> node) {
+            // adds a node to the graph
+            _nodeSet.Add(node);
+        }
+
+        public void AddNode(T value) {
+            // adds a node to the graph
+            _nodeSet.Add(new GraphNode<T>(value));
+        }
+
+        public void AddDirectedEdge(T from, T to) {
+            AddDirectedEdge(from, to, 1);
+        }
+
+        public void AddDirectedEdge(GraphNode<T> from, GraphNode<T> to) {
+            AddDirectedEdge(from, to, 1);
+        }
+
+        public void AddDirectedEdge(T from, T to, int cost) {
+            AddDirectedEdge(new GraphNode<T>(from), new GraphNode<T>(to), cost);
+        }
+
+        public void AddDirectedEdge(GraphNode<T> from, GraphNode<T> to, int cost) {
+            from.Neighbors.Add(to);
+            from.Costs.Add(cost);
+        }
+
+        public void AddUndirectedEdge(T from, T to, int cost) {
+            AddUndirectedEdge(new GraphNode<T>(from), new GraphNode<T>(to), cost);
+        }
+
+        public void AddUndirectedEdge(GraphNode<T> from, GraphNode<T> to, int cost) {
+            from.Neighbors.Add(to);
+            from.Costs.Add(cost);
+
+            to.Neighbors.Add(from);
+            to.Costs.Add(cost);
+        }
+
+        public bool Contains(T value) {
+            return _nodeSet.FindByValue(value) != null;
+        }
+
+        public bool Remove(T value) {
+            // first remove the node from the nodeset
+            GraphNode<T> nodeToRemove = (GraphNode<T>)_nodeSet.FindByValue(value);
+            if (nodeToRemove == null)
+                // node wasn't found
+                return false;
+
+            // otherwise, the node was found
+            _nodeSet.Remove(nodeToRemove);
+
+            // enumerate through each node in the nodeSet, removing edges to this node
+            foreach (GraphNode<T> gnode in _nodeSet) {
+                int index = gnode.Neighbors.IndexOf(nodeToRemove);
+                if (index != -1) {
+                    // remove the reference to the node and associated cost
+                    gnode.Neighbors.RemoveAt(index);
+                    gnode.Costs.RemoveAt(index);
+                }
+            }
+
+            return true;
+        }
+
+        public NodeList<T> Nodes {
+            get {
+                return _nodeSet;
+            }
+        }
+
+        public int Count {
+            get { return _nodeSet.Count; }
+        }
+    }
+}
---
+++ graph/Program.cs
@@ -0,0 +1,30 @@
+﻿using graph.DataStructure;
+
+namespace graph {
+    class Program {
+        static void Main(string[] args) {
+            Graph<string> web = new Graph<string>();
+            web.AddNode("Privacy.htm");
+            web.AddNode("People.aspx");
+            web.AddNode("About.htm");
+            web.AddNode("Index.htm");
+            web.AddNode("Products.aspx");
+            web.AddNode("Contact.aspx");
+            web.AddDirectedEdge("People.aspx", "Privacy.htm");  // People -> Privacy
+
+            web.AddDirectedEdge("Privacy.htm", "Index.htm");    // Privacy -> Index
+            web.AddDirectedEdge("Privacy.htm", "About.htm");    // Privacy -> About
+
+            web.AddDirectedEdge("About.htm", "Privacy.htm");    // About -> Privacy
+            web.AddDirectedEdge("About.htm", "People.aspx");    // About -> People
+            web.AddDirectedEdge("About.htm", "Contact.aspx");   // About -> Contact
+
+            web.AddDirectedEdge("Index.htm", "About.htm");      // Index -> About
+            web.AddDirectedEdge("Index.htm", "Contact.aspx");   // Index -> Contacts
+            web.AddDirectedEdge("Index.htm", "Products.aspx");  // Index -> Products
+
+            web.AddDirectedEdge("Products.aspx", "Index.htm");  // Products -> Index
+            web.AddDirectedEdge("Products.aspx", "People.aspx");// Products -> People
+        }
+    }
+}
