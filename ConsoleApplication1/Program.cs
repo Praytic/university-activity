2f352ac3840dfee021ac2c09acb0a6e89e4c2ddd
praytic <chernogorovvm@gmail.com>
2016-04-22 00:53:50 +0300

    Add threading logic

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApplication1 {
     public class Program {
@@ -11,7 +12,7 @@ namespace ConsoleApplication1 {
             ob2 = temp;
         }
 
-        public static T[] BubbleSort<T>(T[] arrayToSort, SortComparer<T> comparer) {
+        public static T[] BubbleSort2<T>(T[] arrayToSort, SortComparer<T> comparer) {
             for (int i = 0; i < arrayToSort.Length; i++) {
                 for (int j = i; j < arrayToSort.Length; j++) {
                     if (comparer(arrayToSort[i], arrayToSort[j]) > 0) {
@@ -25,12 +26,49 @@ namespace ConsoleApplication1 {
         static void Main(string[] args) {
             var testArrayOfStrings = new[] { "aaa", "aaf", "bba", "aa", "bbbb", "bbba", "z", "zab", "ababab" };
             var testArray = new[] { 2, 4, 5, 1, 0, 10, 30, 14, 3 };
-            Console.WriteLine(string.Join(", ", testArray));
-            Console.WriteLine(string.Join(", ", testArrayOfStrings));
-            var sortedTestArray = BubbleSort(testArray, (ob1, ob2) => (ob1 > ob2) ? 1 : (ob1 == ob2) ? 0 : -1);
-            var sortedTestArrayOfStrings = BubbleSort(testArrayOfStrings, (ob1, ob2) => (ob1.Length > ob2.Length) ? 1 : (ob1.Length == ob2.Length) ? string.Compare(ob1, ob2, StringComparison.Ordinal) : -1);
-            Console.WriteLine(string.Join(", ", sortedTestArray));
-            Console.WriteLine(string.Join(", ", sortedTestArrayOfStrings));
+        
+
+            SortComparer<int> intComparer = (ob1, ob2) => (ob1 > ob2) ? 1 : (ob1 == ob2) ? 0 : -1;
+            SortComparer<string> stringComparer = (ob1, ob2) => (ob1.Length > ob2.Length) ? 1 : (ob1.Length == ob2.Length) ? string.Compare(ob1, ob2, StringComparison.Ordinal) : -1;
+
+            var bubbleSortInt = new BubbleSort<int>(testArray, intComparer);
+            var bubbleSortString = new BubbleSort<string>(testArrayOfStrings, stringComparer);
+
+            Console.WriteLine(string.Join(", ", bubbleSortInt.SortedArray));
+            Console.WriteLine(string.Join(", ", bubbleSortString.SortedArray));
+
+            Thread intSortingThread = new Thread(bubbleSortInt.Run);
+            Thread stringSortingThread = new Thread(bubbleSortString.Run);
+            intSortingThread.Start();
+            stringSortingThread.Start();
+        }
+
+        public class BubbleSort<T>
+        {
+            public T[] SortedArray { get; }
+
+            private delegate void SortCompleteHandler(T[] sortedArray);
+
+            private event SortCompleteHandler SortComplete;
+
+            private readonly SortComparer<T> _comparer; 
+
+            public BubbleSort(T[] sortedArray, SortComparer<T> comparer) {
+                SortedArray = sortedArray;
+                _comparer = comparer;
+                SortComplete = x => Console.WriteLine(string.Join(", ", x));
+            }
+
+            public void Run() {
+                for (int i = 0; i < SortedArray.Length; i++) {
+                    for (int j = i; j < SortedArray.Length; j++) {
+                        if (_comparer(SortedArray[i], SortedArray[j]) > 0) {
+                            Swap(ref SortedArray[i], ref SortedArray[j]);
+                        }
+                    }
+                }
+                SortComplete(SortedArray);
+            }
         }
     }
 }
