85d9ae2d1caae190ffa3567ce6eec64a93775640
praytic <chernogorovvm@gmail.com>
2016-04-22 10:35:56 +0300

    Remove static bubble sorting

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,8 +3,6 @@ using System.Threading;
 
 namespace ConsoleApplication1 {
     public class Program {
-        public delegate int SortComparer<in T>(T ob1, T ob2);
-
         public static void Swap<T>(ref T ob1, ref T ob2)
         {
             var temp = ob1;
@@ -12,23 +10,11 @@ namespace ConsoleApplication1 {
             ob2 = temp;
         }
 
-        public static T[] BubbleSort2<T>(T[] arrayToSort, SortComparer<T> comparer) {
-            for (int i = 0; i < arrayToSort.Length; i++) {
-                for (int j = i; j < arrayToSort.Length; j++) {
-                    if (comparer(arrayToSort[i], arrayToSort[j]) > 0) {
-                        Swap(ref arrayToSort[i], ref arrayToSort[j]);
-                    }
-                }
-            }
-            return arrayToSort;
-        }
-
         static void Main(string[] args) {
             var testArrayOfStrings = new[] { "aaa", "aaf", "bba", "aa", "bbbb", "bbba", "z", "zab", "ababab" };
             var testArray = new[] { 2, 4, 5, 1, 0, 10, 30, 14, 3 };
-
-            SortComparer<int> intComparer = (ob1, ob2) => (ob1 > ob2) ? 1 : (ob1 == ob2) ? 0 : -1;
-            SortComparer<string> stringComparer = (ob1, ob2) => (ob1.Length > ob2.Length) ? 1 : (ob1.Length == ob2.Length) ? string.Compare(ob1, ob2, StringComparison.Ordinal) : -1;
+            Func<int, int, int> intComparer = (ob1, ob2) => (ob1 > ob2) ? 1 : (ob1 == ob2) ? 0 : -1;
+            Func<string, string, int> stringComparer = (ob1, ob2) => (ob1.Length > ob2.Length) ? 1 : (ob1.Length == ob2.Length) ? string.Compare(ob1, ob2, StringComparison.Ordinal) : -1;
 
             var bubbleSortInt = new BubbleSort<int>(testArray, intComparer);
             var bubbleSortString = new BubbleSort<string>(testArrayOfStrings, stringComparer);
@@ -42,21 +28,23 @@ namespace ConsoleApplication1 {
             stringSortingThread.Start();
         }
 
+        private static void Dictionary<T1, T2>() {
+            throw new NotImplementedException();
+        }
+
         public class BubbleSort<T>
         {
             public T[] SortedArray { get; }
+            
+            private readonly Action<T[]> SortComplete;
 
-            private delegate void SortCompleteHandler(T[] sortedArray);
-
-            private event SortCompleteHandler SortComplete;
-
-            private readonly SortComparer<T> _comparer; 
+            private readonly Func<T, T, int> _comparer; 
 
             public BubbleSort() {
                 SortComplete = x => Console.WriteLine(string.Join(", ", x));
             }
 
-            public BubbleSort(T[] sortedArray, SortComparer<T> comparer) : this() {
+            public BubbleSort(T[] sortedArray, Func<T, T, int> comparer) : this() {
                 SortedArray = sortedArray;
                 _comparer = comparer;
             }
